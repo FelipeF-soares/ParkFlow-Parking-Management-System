e7a4ba6c36d2cf738fa0ebfeb56798b9a2c9ea96
FelipeF-soares <felipe.fends@gmail.com>
2024-12-09 19:29:03 -0300

    3- Configured classe DbContext and Created Context

---
+++ ParkFlowData/DataBaseContext/ParkFlowContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkFlowModels.Event;
+using ParkFlowModels.Thing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkFlowData.DataBaseContext;
+
+public class ParkFlowContext : DbContext
+{
+    public ParkFlowContext(DbContextOptions<ParkFlowContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<EntryExitAccess> EntryExitAccesses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Vehicle>()
+                    .HasIndex(vehicle => vehicle.LicensePlate)
+                    .IsUnique();
+
+    }
+}
---
+++ ParkFlowData/Migrations/20241209220905_CreateNewDbParkFlow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkFlowData.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewDbParkFlow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LicensePlate = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Brand = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Model = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Color = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "EntryExitAccesses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EntryTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ExitTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Status = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    VehicleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntryExitAccesses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EntryExitAccesses_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntryExitAccesses_VehicleId",
+                table: "EntryExitAccesses",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_LicensePlate",
+                table: "Vehicles",
+                column: "LicensePlate",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntryExitAccesses");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
